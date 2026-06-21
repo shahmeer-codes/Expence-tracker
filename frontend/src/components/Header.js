83e8668929b9b645ecc8e7d11786356be91ef04f
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-21 19:43:30 +0500

    Add Header component for Expense Tracker

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <h2>Expense Tracker</h2>
+      <p className="subtitle">Manage your finances</p>
+    </header>
+  );
+};
