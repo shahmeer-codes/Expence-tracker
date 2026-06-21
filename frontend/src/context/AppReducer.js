2381d90b5101215c9a7b78fa700ee6a98d106c8b
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-21 19:39:46 +0500

    Remove comments from AppReducer.js

--- frontend/src/context/AppReducer.js
+++ frontend/src/context/AppReducer.js
@@ -1,10 +1,10 @@
-// This reducer handles all global state updates based on dispatched actions
+
 export default (state, action) => {
   switch (action.type) {
     case 'DELETE_TRANSACTION':
       return {
         ...state,
-        // Filter out the transaction with the matching ID
+      
         transactions: state.transactions.filter(
           (transaction) => transaction.id !== action.payload
         ),
@@ -12,13 +12,13 @@ export default (state, action) => {
     case 'ADD_TRANSACTION':
       return {
         ...state,
-        // Add the new transaction to the beginning of the array
+        
         transactions: [action.payload, ...state.transactions],
       };
     case 'SET_FILTER':
       return {
         ...state,
-        // Update the current filter state (ALL, INCOME, EXPENSE)
+        
         filter: action.payload,
       };
     default:
